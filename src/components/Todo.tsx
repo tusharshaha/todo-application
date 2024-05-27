c044b392c1f93933c22f16bf22a31093c3d36759
tusharshaha <tusharshaha128@gmail.com>
2024-05-27 20:45:46 +0600

    installed pretter and husky

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Todo:React.FC = () => {
+const Todo: React.FC = () => {
   return (
-    <div className='todo_container'>
-      <div className="todo_card">
-        <h3>Todo List</h3>
-        <div>
-
+    <div className="bg-gray-300 h-screen pt-4">
+      <div className="bg-white max-w-3xl rounded-md mx-auto p-3">
+        <h1 className="text-center text-3xl font-bold">Todo List</h1>
+        <div className="space-y-3">
+          <p className="font-bold text-xl border-b">Add Item</p>
+          <div className="flex items-center justify-between gap-3">
+            <input
+              type="text"
+              placeholder="add item"
+              className="border-2 py-2 px-6 rounded-md w-full"
+              required
+            />
+            <button className="bg-green-600 btn">Add</button>
+          </div>
         </div>
       </div>
     </div>
