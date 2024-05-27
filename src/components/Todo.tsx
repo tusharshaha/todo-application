901bc297cadb94c7aaa855127141eab4b0d8befc
tusharshaha <tusharshaha128@gmail.com>
2024-05-27 20:26:23 +0600

    installed tailwind css for ui design

---
+++ src/components/Todo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Todo:React.FC = () => {
+  return (
+    <div className='todo_container'>
+      <div className="todo_card">
+        <h3>Todo List</h3>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
